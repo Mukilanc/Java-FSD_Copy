88ea53b33febf832c857d862ce21284089331e93
Mukilanc <83763421+Mukilanc@users.noreply.github.com>
2023-09-14 16:37:28 +0530

    Made changes

--- Practice Project - 1.4/src/TryCatchDemo.java
+++ Practice Project - 1.4/src/TryCatchDemo.java
@@ -1,15 +1,15 @@
 public class TryCatchDemo {
 
     public static void main(String[] args) {
-        // Try to divide 10 by 0.
+
         try {
             int result = 10 / 0;
         } catch (ArithmeticException e) {
-            // Handle the ArithmeticException.
+
             System.out.println("An ArithmeticException occurred: " + e.getMessage());
         }
 
-        // Continue executing the program.
+
         System.out.println("The program continues to execute.");
     }
 }
